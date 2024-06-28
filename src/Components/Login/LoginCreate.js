c9ca3432565731bd37ff61e7344fd92a20bdcfc5
TonioRique <isaque.oliveira2@fatec.sp.gov.br>
2024-06-27 23:30:46 -0300

    finish project

--- src/Components/Login/LoginCreate.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LoginCreate = () => {
-    return (
-        <div>Login Criar</div>
-    )
-}
-
-export default LoginCreate
--- src/Components/Login/LoginPasswordLost.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LoginPassowdLost = () => {
-    return (
-        <div>LoginPassowdLost</div>
-    )
-}
-
-export default LoginPassowdLost
